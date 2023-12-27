2606ee80d7328afb69d660c463e88ccba39f373a
Norbu Delma <nobudelma@gmail.com>
2023-12-27 14:29:11 +0600

    sidebar and header changes

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,84 +5,99 @@ import {
   AiOutlineShopping,
   AiOutlineStock,
   AiOutlineFileText,
+  AiOutlineAppstore,
 } from "react-icons/ai";
 import { BsPlus, BsListUl, BsClipboardData } from "react-icons/bs";
 
 const Sidebar = () => {
   return (
     <Nav
-      className="flex-column sidebar bg-white navbar-light"
+      className="flex-column sidebar bg-white navbar-light ml-0 mr-0"
+      style={{ lineHeight: "1" }}
       // style={{ backgroundColor: "white" }}
     >
       {/* Product Section */}
       <Nav.Item>
-        <Nav.Link href="#" className="fw-bold">
-          <AiOutlineShopping className="ml-2" />
+        <Nav.Link href="#" className="fw-bold pb-0 NavLink">
+          <AiOutlineAppstore className="me-1" size={19} />
+          Dashboard
+        </Nav.Link>
+        <Nav className="ml-3 flex-column sub-list">
+          <hr />
+        </Nav>
+      </Nav.Item>
+
+      {/* Product Section */}
+      <Nav.Item>
+        <Nav.Link href="#" className="fw-bold py-0 NavLink">
+          <AiOutlineShopping className="me-1 mb-1" size={19} />
           Product
         </Nav.Link>
         <Nav className="ml-3 flex-column sub-list">
           <Nav.Item>
-            <Nav.Link href="#" className="ml-3">
+            <Nav.Link href="#" className="ml-3 NavLink">
               Item List
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#" className="ml-3">
+            <Nav.Link href="#" className="ml-3 NavLink">
               Add Item
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#" className="ml-3">
+            <Nav.Link href="#" className="ml-3 NavLink">
               Category List
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#" className="ml-3">
+            <Nav.Link href="#" className="ml-3 NavLink">
               Add Category
             </Nav.Link>
           </Nav.Item>
+          <hr />
         </Nav>
       </Nav.Item>
 
       {/* Transaction Section */}
       <Nav.Item>
-        <Nav.Link href="#" className="fw-bold">
-          <AiOutlineStock className="mr-2" />
+        <Nav.Link href="#" className="fw-bold py-0 NavLink">
+          <AiOutlineStock className="me-1 mb-1" size={19} />
           Transaction
         </Nav.Link>
         <Nav className="ml-3 flex-column sub-list">
           <Nav.Item>
-            <Nav.Link href="#" className="ml-3">
+            <Nav.Link href="#" className="ml-3 NavLink">
               Transaction History
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#" className="ml-3">
+            <Nav.Link href="#" className="ml-3 NavLink">
               Stock In
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#" className="ml-3">
+            <Nav.Link href="#" className="ml-3 NavLink">
               Stock Out
             </Nav.Link>
           </Nav.Item>
+          <hr />
         </Nav>
       </Nav.Item>
 
       {/* Report Section */}
       <Nav.Item>
-        <Nav.Link href="#" className="fw-bold">
-          <AiOutlineFileText className="mr-2" />
+        <Nav.Link href="#" className="fw-bold py-0">
+          <AiOutlineFileText className="me-1 mb-1" size={19} />
           Report
         </Nav.Link>
         <Nav className="ml-3 flex-column sub-list">
           <Nav.Item>
-            <Nav.Link href="#" className="ml-3">
+            <Nav.Link href="#" className="ml-3 NavLink">
               Inventory Report
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#" className="ml-3">
+            <Nav.Link href="#" className="ml-3 NavLink">
               Schedule Report
             </Nav.Link>
           </Nav.Item>
