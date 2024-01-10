9f30f931779bd80d096e3c1e5d36a58511b9480d
Kinley <kinleyzangmo407@gmail.com>
2024-01-10 10:50:20 +0600

    Update constant.jsx

--- frontend/src/constant.jsx
+++ frontend/src/constant.jsx
@@ -1,5 +1,5 @@
 // export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://192.168.131.147:3001' : '';
-export const BASE_URL = 'http://192.168.131.121:3001';
+export const BASE_URL = 'http://192.168.131.154:3001';
 
 export const CATEGORY_URL = '/api/category';
 export const ITEM_URL = '/api/item';
