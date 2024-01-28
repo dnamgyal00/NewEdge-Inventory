1118ac27b5ec574efb82d70258af56f79e0de18c
Norbu Delma <nobudelma@gmail.com>
2024-01-28 18:42:14 +0600

    profile changes

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -4,15 +4,19 @@ import testImage from "../assets/warehouse.png";
 
 const ProfileScreen = () => {
   return (
-    <div className="col-sm-8 col-xl-6 bg-white m-auto">
-      <div className="align-items-center">
+    <div className="col-sm-8 col-xl-6 bg-white m-auto py-3">
+      <div className="d-flex justify-content-center">
         <Image
           fluid
           src={testImage}
           className=" shadow-lg rounded-circle w-50 "
         ></Image>
       </div>
-      <div>Norbu Delma</div>
+      <div className="d-flex flex-column align-items-center pt-4">
+        <h6>Inventory Manager</h6>
+
+        <p>Admin</p>
+      </div>
     </div>
   );
 };
