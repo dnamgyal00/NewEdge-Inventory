ad3d013815559753e5a3da35ff686c933f0a1ec2
Norbu Delma <nobudelma@gmail.com>
2024-01-28 18:36:05 +0600

    profile and search results

--- frontend/src/screens/ItemScreen.jsx
+++ frontend/src/screens/ItemScreen.jsx
@@ -3,7 +3,10 @@ import Table from "react-bootstrap/Table";
 import { FaPlus, FaSearch, FaTimes } from "react-icons/fa";
 import { FiFilter, FiEdit3 } from "react-icons/fi";
 import { LinkContainer } from "react-router-bootstrap";
-import { useGetItemsQuery ,useSearchItemByNameQuery } from "../slices/itemsApiSlice";
+import {
+  useGetItemsQuery,
+  useSearchItemByNameQuery,
+} from "../slices/itemsApiSlice";
 import { useGetCategoriesQuery } from "../slices/categoriesApiSlice";
 
 import Loader from "../components/Loader";
@@ -37,7 +40,7 @@ const ItemScreen = () => {
     setCategoryName("");
   };
 
-  const [search,setSearch]=useState("");
+  const [search, setSearch] = useState("");
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setSearch(value);
@@ -57,8 +60,8 @@ const ItemScreen = () => {
     isLoading2,
     isError2,
     error2,
-  } = useSearchItemByNameQuery (search?search:"1");
-  console.log(itemSearchResults)
+  } = useSearchItemByNameQuery(search ? search : "1");
+  console.log(itemSearchResults);
 
   const {
     data: { data: categories } = {},
@@ -105,7 +108,7 @@ const ItemScreen = () => {
           </div>
 
           {/* Search Bar */}
-          <div className="border border-solid d-flex py-0 rounded">
+          <div className="d-flex py-0 border border-solid d-flex rounded">
             <span className="input-group-text bg-white border-0">
               <FaSearch />
             </span>
@@ -113,23 +116,33 @@ const ItemScreen = () => {
               type="text"
               placeholder="Search..."
               className="form-control border-0 px-0 py-0"
-              name='search'
+              name="search"
               value={search}
               onChange={handleInputChange}
               style={{ boxShadow: "none" }}
             />
+            <div className="search-results bg-white position-absolute top-100  translate-middle-x px-3 mt-2">
+              {itemSearchResults &&
+                itemSearchResults.map((result) => (
+                  <LinkContainer
+                    key={result.id}
+                    to={{ pathname: `/home/item/${result.name}` }}
+                    onClick={() => dispatch(setItemId(result.id))}
+                  >
+                    <div
+                      key={result.id}
+                      className="border-0 clickable-cell py-1"
+                    >
+                      {" "}
+                      {result.name}{" "}
+                    </div>
+                  </LinkContainer>
+                ))}
+            </div>
 
+            {/* EDIT THIS SEARCH RESULT DISPLAY */}
           </div>
-          
         </div>
-         {/* EDIT THIS SEARCH RESULT DISPLAY */}
-         {itemSearchResults && itemSearchResults.map((result)=>(
-              <LinkContainer key={result.id} to={{ pathname: `/home/item/${result.name}`}}
-                    onClick={() => dispatch(setItemId(result.id))}>
-                    <div key={result.id} className="border-0 "> {result.name} </div> 
-             </LinkContainer>
-            )) 
-            }
 
         {/* Filter Options*/}
         <div className="input-group mb-3  ">
--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Image } from "react-bootstrap";
+import testImage from "../assets/warehouse.png";
 
 const ProfileScreen = () => {
   return (
-    <div>ProfileScreen</div>
-  )
-}
+    <div className="col-sm-8 col-xl-6 bg-white m-auto">
+      <div className="align-items-center">
+        <Image
+          fluid
+          src={testImage}
+          className=" shadow-lg rounded-circle w-50 "
+        ></Image>
+      </div>
+      <div>Norbu Delma</div>
+    </div>
+  );
+};
 
-export default ProfileScreen
+export default ProfileScreen;
